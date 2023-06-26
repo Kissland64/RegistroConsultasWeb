af57e40ee6c3c27676b703174cabe80e526b4fcd
Kissland Baker <Kissland_baker@ucne.edu.do>
2023-06-25 22:52:25 -0400

    Agregando los modelos, Context y los Controllers

---
+++ Server/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroConsultasWeb.Shared.Models;
+
+namespace ResgistrosConsultasWebAssembly.Server.DAL
+{
+    public class Context : DbContext
+    {
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Prioridades> Prioridades { get; set; }
+        public DbSet<Tickets> Tickets { get; set; }
+        public DbSet<Sistemas> Sistemas { get; set; }
+        public Context(DbContextOptions<Context> options) : base(options) { }
+    }
+}
